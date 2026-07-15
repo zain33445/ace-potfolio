aaa0364db5f50975d426d0946022c9657d71b81f
zain33445 <zain33445@users.noreply.github.com>
2026-07-15 20:19:22 +0500

    fix(why-choose): shift bridge animation 4% right

--- src/components/Home/sections/WhyChooseUsSection.tsx
+++ src/components/Home/sections/WhyChooseUsSection.tsx
@@ -478,7 +478,7 @@ export default function WhyChooseUsSection() {
           {isDesktop ? (
             <>
               <div
-                className="relative w-[95%] max-w-[1400px]"
+                className="relative w-[95%] max-w-[1400px] translate-x-[4%]"
                 style={{ aspectRatio: '1200 / 900' }}
               >
                 <svg
